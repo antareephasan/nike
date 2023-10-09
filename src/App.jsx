0275a4d2e4d2a91757439ab9f4304ef4d852c459
Antareep Hasan <antareephasan@gmail.com>
2023-10-09 06:23:00 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+const App = () => (
+  <main className="relative">
+    Nav
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      Hero
+    </section>
+    <section className="padding">
+      PopularProducts
+    </section>
+    <section className="padding">
+      SuperQuality
+    </section>
+    <section className="padding-x py-10">
+      Services
+    </section>
+    <section className="padding">
+      SpecialOffer
+    </section>
+    <section className="padding">
+      CustomerReviews
+    </section>
+    <section className="padding">
+      Subscribe
+    </section>
+    <section>
+      Footer
+    </section>
+  </main>
+);
+
+export default App;
